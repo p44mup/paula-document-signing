12522a87d4c047eebcb58dfcbb8fd9513f3d1e49
p44mup <paulawellington@hotmail.com>
2025-06-22 18:54:14 +0100

    Update layout.tsx

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,27 +1,21 @@
-'use client'
-
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: "Paula's Document Signing",
+  description: 'RentSmart Wales Property Management',
+}
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const [supabase] = useState(() => createClientComponentClient())
-
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <SessionContextProvider supabaseClient={supabase}>
-          {children}
-        </SessionContextProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
